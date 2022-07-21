1bd3d4c2d2ab4199d4a371471f9eff018e668b54
tsots <mosalatsotetsi@gmail.com>
2022-07-21 09:01:33 +0200

    Updates

---
+++ _next/static/begxowNiHbQIdd80UhA24/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-382014836f3ac81e.js"],"/_error":["static/chunks/pages/_error-62bad7beda3241e7.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
